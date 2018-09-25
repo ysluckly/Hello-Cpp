3df9e3100886550a1387d5313b89a371b82da47e
Mr-Hunter <1522138135@qq.com>
2018-09-25 23:20:22 +0800

    排序算法大全

---
+++ 【四大排序算法】大全/Sort.h
@@ -0,0 +1,33 @@
+#ifndef __INSERTSORT_H__
+#define __INSERTSORT_H__
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+//≤Â»Î≈≈–Ú
+//1.÷±Ω”≤Â»Î≈≈–Ú
+void InsertSort(int* arr,int n);
+//2.œ£∂˚≈≈–Ú
+void ShellSort(int* arr,int n);
+
+
+//—°‘Ò≈≈–Ú
+//1.—°‘Ò≈≈–Ú
+void SelectSort(int* arr, int n);
+//2.∂—≈≈–Ú
+void HeapSort(int* arr,int n);
+
+
+//Ωªªª≈≈–Ú
+//1.√∞≈›≈≈–Ú
+void BubbleSort(int* arr,int n);
+//2.øÏÀŸ≈≈–Ú
+void QuickSort(int* arr,int begin,int end);
+
+//πÈ≤¢≈≈–Ú
+void MergeSort(int* arr, int n);
+
+
+#endif //__INSERTSORT_H__
---
+++ 【四大排序算法】大全/test.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "Sort.h"
+
+
+
+int main()
+{
+	 
+	int arr[] = { 2, 3, 8, 1, 23, 56, 848, 21, 15 };
+	int len = sizeof(arr) / sizeof(arr[0]);
+
+	/*InsertSort(arr,len);
+	Print(arr, len);
+	ShellSort(arr,len);
+	Print(arr, len);
+
+	SelectSort(arr,len);
+	Print(arr, len);
+	HeapSort(arr, len);
+	Print(arr, len);
+
+	BubbleSort(arr, len);
+	Print(arr, len);*/
+
+	QuickSort(arr, 0,len-1);
+	Print(arr, len);
+
+	MergeSort(arr, len);
+	Print(arr, len);
+
+	system("pause");
+	return 0;
+}
+
