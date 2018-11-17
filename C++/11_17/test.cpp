fb20b2f17ba305565110eac3eb561bfaccb9b9ef
Mr-Hunter <1522138135@qq.com>
2018-11-17 23:39:00 +0800

    在栈上，堆上创建对象，单例模式

---
+++ C++/11_17/test.cpp
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+#include <Windows.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+
+//只能在栈上创建对象
+void test1()
+{
+	class HeapOnly
+	{
+	public:
+		 
+	};
+
+}
+//只能在堆上创建对象
+void test2()
+{
+
+}
+//传统模拟实现string
+void test3()
+{
+
+}
+//现代模拟实现string
+void test4()
+{
+
+}
+using namespace std;
+int main()
+{
+	//test1();
+	//test2();
+	//test3();
+	//test4();
+
+	system("pause");
+	return 0;
+}
+
